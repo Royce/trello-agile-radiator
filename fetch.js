36bae726d52d088b716f6ef6df9d4d7e59ab0e7e
Royce Townsend <github@roycetownsend.com>
2013-07-24 17:17:46 +0800

    Protect the app key etc.

--- fetch.js
+++ fetch.js
@@ -1,10 +1,7 @@
-var config = {
-  app_id: '38e3c9586d9a74c086ec41d48aa14d96'
-  , app_key: '4d2a0525bdb0cf88ac8d6bf5fc22d3365e20cb8f52e6666b932d2c76440ec45c'
-};
+var config = require('./config');
 
-var tragic = require('./lib/');
-var f = new tragic.Fetcher(config);
-var data = f.fetch('4f7696a77aaa0177051298fb');
+var tragic = require('./lib');
+var f = new tragic.Fetcher(config.app_id, config.app_key);
+var data = f.fetch(config.board);
 
 console.log(data[0]);
